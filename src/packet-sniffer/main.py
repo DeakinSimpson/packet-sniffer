9e150772e5acff170814d3c292e2209b6c7dc591
DeakinSimpson <deakinsimpson976@gmail.com>
2026-07-15 19:27:31 +1000

    fix: updated network_sniffer to network_tools in main

--- src/packet-sniffer/main.py
+++ src/packet-sniffer/main.py
@@ -2,16 +2,16 @@
 
 import sys
 from scapy.all import *
-import network_sniffer
+import network_tools
 
 
 def main():
     output_packet_details = []
 
-    sniff(count= 3, prn=lambda p:network_sniffer.analysePacket(p, output_packet_details))
+    sniff(count= 3, prn=lambda p:network_tools.analysePacket(p, output_packet_details))
 
-    network_sniffer.printPacketDetailsInline(output_packet_details)
-    network_sniffer.printFullPacketDetails(output_packet_details)
+    network_tools.printPacketDetailsInline(output_packet_details)
+    network_tools.printFullPacketDetails(output_packet_details)
     
 
 if __name__ ==  "__main__":
