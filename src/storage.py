a96c4f2f0132b6920e04b3650470060beb5f306b
DeakinSimpson <deakinsimpson976@gmail.com>
2026-07-15 21:34:17 +1000

    feat: added create table function to create table if none exist

--- src/storage.py
+++ src/storage.py
@@ -6,7 +6,7 @@ import os
 if not os.path.exists('data'):
     os.makedirs('data')
 
-
+# creates the initial db
 def db_init():
     try:
         # initialise db connection
@@ -15,6 +15,23 @@ def db_init():
 
         # inialise curser
         curser = sqliteConnection.cursor()
+
+        # create table if one doesnt already exits
+        create_table_query = """
+            CREATE TABLE IF NOT EXISTS PACKETS (
+                id integer PRIMARY KEY AUTOINCREMENT,
+                time REAL,
+                src TEXT,
+                dst TEXT,
+                proto INTEGER,
+                sport INTEGER,
+                dport INTEGER,
+                size INTEGER,
+                dns_query TEXT,
+                raw TEXT
+            );
+        """
+        curser.execute(create_table_query)
 
     except sqlite3.Error as error:
         print('Error occured - ', error)
